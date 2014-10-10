20c9939ed6ef966ab0b4f5f37b7049b5125f47d8
Stas Volskyi <admin@Mac-Admin.local>
2014-10-10 19:21:10 +0300

    fixed bugs

--- Periop/Constants/Constants.h
+++ Periop/Constants/Constants.h
@@ -6,7 +6,7 @@
 //  Copyright (c) 2014 Thinkmobiles. All rights reserved.
 //
 
-static NSString *const FONT_MuseoSans700 = @"MuseoSans_700";
+static NSString *const FONT_MuseoSans700 = @"MuseoSans-700";
 static NSString *const FONT_MuseoSans300 = @"MuseoSans-300";
 static NSString *const FONT_MuseoSans100 = @"MuseoSans-100";
 static NSString *const FONT_MuseoSans500 = @"MuseoSans-500";
